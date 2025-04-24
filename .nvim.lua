04b28b4488d5a5c1381d1f4cbd6505ffec832627
mt <descentcare@gmail.com>
2025-04-24 23:15:47 +0500

    session

--- .nvim.lua
+++ .nvim.lua
@@ -1,3 +1,2 @@
 vim.keymap.set('n', '<Leader>fs', '<cmd>lua require"fzf-lua".fzf_live("./filtercs.py <query> 2>/dev/null", { prompt="scs> " , exec_empty_query=true })<cr>')
 vim.keymap.set('n', '<Leader>cs', '<cmd>!python calcchar.py<cr><cr>')
-vim.cmd([[colorscheme retrobox]])
